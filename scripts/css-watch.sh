5882ab2dd2c1002aa8cf912898ad648927c5114a
Bladimir Cabrera <vladwithcode@gmail.com>
2025-03-06 16:03:44 -0600

    chore: update tailwind tranpile script for v4
    Use bun to transpile tailwind and update project config for tailwind v4

--- scripts/css-watch.sh
+++ scripts/css-watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npx tailwindcss -i web/styles/styles.css -o web/static/styles.css -c "web/styles/tailwind.config.js" --watch
+bunx tailwindcss -i web/styles/styles.css -o web/static/styles.css --watch
 
